14794f14b4d9b3579e554530a42c9e71e1c181ea
Valeryi <valerabudkov20@gmail.com>
2024-03-09 01:32:02 +0300

    4

--- currency.py
+++ currency.py
@@ -2,27 +2,26 @@ import requests
 
 ruble = []
 
-data = requests.get('https://free.currconv.com/api/v7/convert?apiKey=d9371a2ba6535eab179c=USD_RUB&compact=ultra').json()
-print(data)
+data = requests.get('https://free.currconv.com/api/v7/convert?q=USD_RUB&compact=ultra&apiKey=d9371a2ba6535eab179c').json()
 ruble.append(data['USD_RUB']) 
 
-data = requests.get('https://free.currconv.com/api/v7/convert?apiKey=d9371a2ba6535eab179c=RUB_KZT&compact=ultra').json()
+data = requests.get('https://free.currconv.com/api/v7/convert?q=RUB_KZT&compact=ultra&apiKey=d9371a2ba6535eab179c').json()
 ruble.append(data['RUB_KZT']) 
 
-data = requests.get('https://free.currconv.com/api/v7/convert?apiKey=d9371a2ba6535eab179c=EUR_RUB&compact=ultra').json()
+data = requests.get('https://free.currconv.com/api/v7/convert?q=EUR_RUB&compact=ultra&apiKey=d9371a2ba6535eab179c').json()
 ruble.append(data['EUR_RUB']) 
 
-data = requests.get('https://free.currconv.com/api/v7/convert?apiKey=d9371a2ba6535eab179c=CNY_RUB&compact=ultra').json()
+data = requests.get('https://free.currconv.com/api/v7/convert?q=CNY_RUB&compact=ultra&apiKey=d9371a2ba6535eab179c').json()
 ruble.append(data['CNY_RUB']) 
 
-data = requests.get('https://free.currconv.com/api/v7/convert?apiKey=d9371a2ba6535eab179c=ILS_RUB&compact=ultra').json()
+data = requests.get('https://free.currconv.com/api/v7/convert?q=ILS_RUB&compact=ultra&apiKey=d9371a2ba6535eab179c').json()
 ruble.append(data['ILS_RUB'])
 
-data = requests.get('https://free.currconv.com/api/v7/convert?apiKey=d9371a2ba6535eab179c=RUB_JPY&compact=ultra').json()
+data = requests.get('https://free.currconv.com/api/v7/convert?q=RUB_JPY&compact=ultra&apiKey=d9371a2ba6535eab179c').json()
 ruble.append(data['RUB_JPY']) 
 
-data = requests.get('https://free.currconv.com/api/v7/convert?apiKey=d9371a2ba6535eab179c=GBP_RUB&compact=ultra').json()
+data = requests.get('https://free.currconv.com/api/v7/convert?q=GBP_RUB&compact=ultra&apiKey=d9371a2ba6535eab179c').json()
 ruble.append(data['GBP_RUB']) 
 
-data = requests.get('https://free.currconv.com/api/v7/convert?apiKey=d9371a2ba6535eab179c=INR_RUB&compact=ultra').json()
+data = requests.get('https://free.currconv.com/api/v7/convert?q=INR_RUB&compact=ultra&apiKey=d9371a2ba6535eab179c').json()
 ruble.append(data['INR_RUB'])
